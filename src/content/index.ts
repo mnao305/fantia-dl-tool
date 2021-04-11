b5a3110f13ff145e717d87403117d6ad1b0e5168
mnao305 <mail@mnao305.com>
2021-04-11 18:10:42 +0900

    feat: ボタンを埋め込む
    まだ何もしないただのボタン

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,1 +1,40 @@
 import { browser } from 'webextension-polyfill-ts'
+
+const btnCreate = () => {
+  const btn = document.createElement('button')
+  btn.textContent = 'Save'
+  return btn
+}
+
+const injectBtn = () => {
+  const btn = btnCreate()
+  const el = document.getElementsByClassName('post-btns')
+  el[0].appendChild(btn)
+}
+
+const main = () => {
+  const target = document.getElementById('page')
+  if (!target) return
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach(mutation => {
+      if ((mutation.target as HTMLElement).className === 'post-btns' && (mutation.addedNodes[0] as HTMLElement).nodeName === 'POST-LIKE-BUTTON') {
+        injectBtn()
+        observer.disconnect()
+      }
+    })
+  })
+
+  const config = {
+    characterData: false,
+    childList: true,
+    subtree: true,
+  }
+
+  observer.observe(target, config)
+
+  window.addEventListener('DOMContentLoaded', () => {
+    console.log('DOMContentLoaded')
+    injectBtn()
+  })
+}
+main()
