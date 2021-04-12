3d475c028d1abd63acf007a825a1bf33a8001486
mnao305 <mail@mnao305.com>
2021-04-12 21:01:35 +0900

    feat: ボタンのCSS変更

--- src/content/index.ts
+++ src/content/index.ts
@@ -181,7 +181,7 @@ let num = 0
 const btnCreate = () => {
   const btn = document.createElement('button')
   btn.textContent = '一括DL'
-  btn.setAttribute('style', 'display: block;margin: 20px auto;')
+  btn.setAttribute('style', 'display: block;margin: 20px auto;padding: 20px 40px;background-color: #22c283;border: none;color: #fff;border-radius: 10px;')
   btn.setAttribute('gallery-count', String(num))
   num++
   btn.onclick = saveImages
