c1f68d2c3339cf471e63397c933826e345faec91
mnao305 <mail@mnao305.com>
2021-05-26 01:18:24 +0900

    feat: 保存形式を階層構造のフォルダに直接ダウンロードするように変更 (#70)
    * feat: 保存形式を階層構造のフォルダに直接ダウンロードするように変更
    
    zipで圧縮せずに直接画像を保存する
    
    BREAKING CHANGE: 保存形式が大幅に変わります
    Closes #68
    
    * feat: 各コンテンツごとのタイトルも使用する
    
    * refactor: 不使用変数の削除

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,1 +1,25 @@
-import { browser } from 'webextension-polyfill-ts'
+import { browser, Downloads } from 'webextension-polyfill-ts'
+
+export const download = (url: string, filename: string, filepath: string): void => {
+  const options: Downloads.DownloadOptionsType = {
+    url: url,
+    filename: `fantia/${filepath}/${filename}`,
+    saveAs: false,
+    conflictAction: 'overwrite',
+  }
+
+  browser.downloads.download(options)
+}
+
+interface DownloadEventMsg {
+  msg: 'download'
+  url: string
+  filepath: string
+  filename: string
+}
+
+browser.runtime.onMessage.addListener((msg: DownloadEventMsg) => {
+  if (msg.msg === 'download') {
+    download(msg.url, msg.filename, msg.filepath)
+  }
+})
