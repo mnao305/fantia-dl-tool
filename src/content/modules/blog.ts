b92f0c4bb05b26c2d584755fbcbdc58aaa35f177
mnao305 <mail@mnao305.com>
2022-07-04 00:58:05 +0900

    fix: 外部の画像を参照しているblogタイプコンテンツでエラーが出るのを修正

--- src/content/modules/blog.ts
+++ src/content/modules/blog.ts
@@ -8,9 +8,13 @@ export const blogDL = (postContent: PostContentBlog, filepath: string): void => 
   const blog = json.ops
   for (let i = 0; i < blog.length; i++) {
     const element = blog[i]
-    if (typeof element.insert !== 'string') {
+    if (typeof element.insert !== 'string' && element.insert.fantiaImage) {
+      // Fantiaの画像
       const url = `https://fantia.jp/${element.insert.fantiaImage.original_url}`
       fileDownload(url, filepath, element.insert.fantiaImage.id + urlToExt(element.insert.fantiaImage.url))
+    } else if (typeof element.insert !== 'string' && element.insert.image) {
+      // 外部を参照している画像
+      fileDownload(element.insert.image, filepath, String(i) + urlToExt(element.insert.image))
     }
   }
   // TODO: テキストのダウンロード
