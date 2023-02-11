47b6acead1cf9d50e2b2bfb858bb3c0790b1f88d
あると <mail@twinkfrag.net>
2023-02-11 18:15:53 +0900

    fix: API呼び出しにTokenが必要になったことに対応 (#357)
    Co-authored-by: mnao305 <mail@mnao305.com>

--- src/content/modules/backnumberPage.ts
+++ src/content/modules/backnumberPage.ts
@@ -5,7 +5,8 @@ export const fetchBacknumberData = async (): Promise<Backnumber> => {
   const url = new URL((document.getElementsByClassName('tab-item tab-item-text active')[1] as HTMLAnchorElement).href)
   const plan = url.searchParams.get('plan')
   const month = url.searchParams.get('month')
-  const json = await ky.get(`https://fantia.jp/api/v1/fanclub/backnumbers/monthly_contents/plan/${plan}/month/${month}`).json<BacknumberResponse>()
+  const csrfToken = document.getElementsByName('csrf-token')[0].getAttribute('content') ?? ''
+  const json = await ky.get(`https://fantia.jp/api/v1/fanclub/backnumbers/monthly_contents/plan/${plan}/month/${month}`, { headers: { 'x-csrf-token': csrfToken } }).json<BacknumberResponse>()
 
   return json.backnumber
 }
