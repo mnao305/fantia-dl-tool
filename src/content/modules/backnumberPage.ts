805d34189b385f93bbfb36d3d2b5dedba568d982
mnao305 <mail@mnao305.com>
2023-07-06 21:25:09 +0900

    fix: APIリクエストにx-requested-withヘッダーを追加 (#428)
    Fantia側の変更対応
    
    #427

--- src/content/modules/backnumberPage.ts
+++ src/content/modules/backnumberPage.ts
@@ -1,12 +1,11 @@
-import ky from 'ky'
 import { Backnumber, BacknumberResponse } from '../../types/backnumber'
+import { get } from './api'
 
 export const fetchBacknumberData = async (): Promise<Backnumber> => {
   const url = new URL((document.getElementsByClassName('tab-item tab-item-text active')[1] as HTMLAnchorElement).href)
   const plan = url.searchParams.get('plan')
   const month = url.searchParams.get('month')
-  const csrfToken = document.getElementsByName('csrf-token')[0].getAttribute('content') ?? ''
-  const json = await ky.get(`https://fantia.jp/api/v1/fanclub/backnumbers/monthly_contents/plan/${plan}/month/${month}`, { headers: { 'x-csrf-token': csrfToken } }).json<BacknumberResponse>()
+  const json = await get<BacknumberResponse>(`https://fantia.jp/api/v1/fanclub/backnumbers/monthly_contents/plan/${plan}/month/${month}`)
 
   return json.backnumber
 }
